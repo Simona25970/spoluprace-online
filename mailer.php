265389e17313bbfa7d3fff7d2d6e573b591cbf7b
Simona Florusová <florusova.simona@seznam.cz>
2026-04-28 16:06:02 +0200

    Add page

---
+++ mailer.php
@@ -0,0 +1,46 @@
+<?php
+// Načítá pole z formuláře - name, email a message; odstraňuje bílé znaky; odstraňuje HTML
+$name = strip_tags(trim($_POST["name"]));
+$name = str_replace(array("\r","\n"),array(" "," "),$name);
+$email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+$message = trim($_POST["message"]);
+
+// Kontroluje data popř. přesměruje na chybovou adresu
+if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+header("Location: http://www.spolupraceonline.info/index.html?success=-1#form");
+exit;
+}
+
+// Nastavte si email, nakterý chcete, aby se vyplněný formulář odeslal - jakýkoli váš email
+$recipient = "simonapracezdomova@seznam.cz";
+
+// Nastavte předmět odeslaného emailu
+$subject = "Máte nový kontakt od: $name";
+
+// Obsah emailu, který se vám odešle
+$email_content = "Jméno: $name\n";
+$email_content .= "Email: $email\n\n";
+$email_content .= "Telefon: $phone\n\n";
+$email_content .= "Zpráva:\n$message\n";
+
+// Emailová hlavička
+$email_headers = "From: $name <$email>";
+
+     // Odeslání emailu - dáme vše dohromady
+     mail($recipient, $subject, $email_content, $email_headers);
+
+     // Přesměrování na stránku, pokud vše proběhlo v pořádku
+     header("Location:http://www.spolupraceonline.info/index.html?success=1#form");
+
+     ?>
+
+<?php
+if($_GET['success'] == 1){
+     echo"<div class=\"form-result success\">Odeslání proběhlo v pořádku</div>";  
+ };
+
+if($_GET['success'] == -1){
+     echo"<div class=\"form-result error\">Chyba při odeslání. Zkuste znovu</div>";
+ };
+
+?>
